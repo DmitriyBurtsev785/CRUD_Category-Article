8b9583c1067ebcc9ae86f4ae11e5ed4aca7c8232
DmitriyBurtsev <dm.burcev@yandex.ru>
2023-04-13 09:13:37 +0300

    Finished class Article. It works

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class Article:
         data = {'slug': slug, 'title': title, 'description': description, 'meta_description': meta_description,
                 'meta_keywords': meta_keywords, 'text': text, 'category': category}
         response = requests.post(request_url, data)
-        print(response.json())
+        # print(response.json())
 
 
     def patch_article_partial_update(self, slug, title, description, meta_description, meta_keywords, text, id):
@@ -78,6 +78,13 @@ class Article:
                 'meta_keyword': meta_keywords, 'text': text, 'id': id}
         response = requests.patch(request_url, data)
         print(response.json())
+
+
+    def get_article_list(self):
+        """GET /blog/article/ - list"""
+        request_url = self.base_host + self.uri
+        response = requests.get(request_url)
+        pprint(response.json())
 
 
     def get_article_retrieve(self, id):
@@ -124,24 +131,12 @@ if __name__ == '__main__':
 
     # СМОТРИМ КОНКРЕТНУЮ КАТЕГОРИЮ
 
-    # category.get_category_retrieve(21)
+    # category.get_category_retrieve(24)
 
 
     # УДАЛЯЕМ КАТЕГОРИИ
 
-    # category.delete_category_destroy(23)
-
-    # ДОБАВЛЯЕМ КАТЕГОРИИ
-
-    # ДОБАВЛЯЕМ КАТЕГОРИИ
-
-    # ДОБАВЛЯЕМ КАТЕГОРИИ
-
-    # ДОБАВЛЯЕМ КАТЕГОРИИ
-
-
-
-
+    # category.delete_category_destroy(111111)
 
 
 
@@ -149,3 +144,55 @@ if __name__ == '__main__':
 
 
     article = Article()
+
+    # ДОБАВЛЯЕМ СТАТЬЮ
+
+    # article.post_article_create('JS', 'JavaScript', 'Полноценный динамический язык программирования',
+    #                                 'JavaScript', 'JavaScript', 'Его разработал Brendan Eich, сооснователь проекта \
+    #                                 Mozilla, Mozilla Foundation и Mozilla Corporation', 21)
+    #
+    # article.post_article_create('Html', 'Html', 'Язык программирования для создания электронных документов',
+    #                                 'Html', 'Html', 'это язык программирования для создания электронных документов, \
+    #                                  называемых страницами, размещаемыми в Интернете. Каждая страница имеет несколько \
+    #                                  подключений к гиперссылкам или ссылкам на другие страницы.', 21)
+    #
+    #
+    # article.post_article_create('Python', 'Python', 'Язык программирования общего назначения',
+    #                                 'Python', 'Python', 'Это высокоуровневый, объектно-ориентированный, \
+    #                                 интерпретируемый язык бэкенда с динамической семантикой.', 22)
+    #
+    #
+    #
+    # article.post_article_create('PHP', 'PHP', 'Один из лучших языков общего назначения для веб-разработки',
+    #                                 'PHP', 'PHP', 'Это скриптовый язык общего назначения, предназначенный\
+    #                                  в основном для веб-разработки.', 22)
+    #
+    #
+    # article.post_article_create('test', 'test', 'Этот блок в разработке',
+    #                                 'test', 'test', 'Информация дополняется', 24)
+
+
+    # ОБНОВЛЯЕМ СТАТЬЮ
+
+
+    # article.patch_article_partial_update('test - обновленный', 'test - обновленный', 'Этот блок в разработке',
+    #                                 'test - обновленный', 'test - обновленный', 'Информация дополняется', 18)
+
+
+
+
+    # СМОТРИМ СПИСОК СТАТЕЙ
+
+
+    # article.get_article_list()
+
+
+    # СМОТРИМ КОНКРЕТНУЮ СТАТЬЮ
+
+    # article.get_article_retrieve(1111111)
+
+
+
+    # УДАДЯЕМ СТАТЬЮ
+
+    # article.delete_article_destroy(1111111111)
